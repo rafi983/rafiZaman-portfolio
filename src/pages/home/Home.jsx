b4155f18bd689ab7e2f92f919b1453509f0c71a8
Rafi Zaman <rafiirfan211@gmail.com>
2024-07-09 13:52:58 +0600

    typo-fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className='home__content'>
         <div className='home__data'>
           <h1 className='home__title'>
-            <span>I'm Rafi Zaman.</span> Web Deeloper
+            <span>I'm Rafi Zaman.</span> Web Developer
           </h1>
 
           <p className='home__description'>
